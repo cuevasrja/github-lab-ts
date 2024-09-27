f1321c4e72e64f8d12bd193e46676b5c2388a1b0
Juan Cuevas y Anya Marcano <19-10056@usb.ve>
2024-09-27 14:58:09 -0400

    Fixing typing and import errors

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,17 +1,16 @@
 // Endpoint for querying the fibonacci numbers
-
+import { Request, Response } from "express";
 import fibonacci from "./fib";
-// import fibonacci from "./fib";
 
-export default (req: any, res: any) => {
-  const num: string = req.params.num;
+export default (req: Request, res: Response) => {
+  const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  const fibN: number = fibonacci(parseInt(num));
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
   }
 
   res.send(result);
-};
+}
